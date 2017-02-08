fe6f26115cc505586665f3e447c73557b749c1c2
Anthony <moz4r@free.fr>
2017-02-08 21:34:51 +0100

    attachement

--- InmoovScript/inmoovCustom/Inmoov_custom.py
+++ InmoovScript/inmoovCustom/Inmoov_custom.py
@@ -19,6 +19,12 @@ ear.addCommand("regarde devant", "python", "regardDevant")
 ear.addCommand("regarde vers la gauche", "python", "regardGauche")
 	
 def paupiereOuvre():
+	paupiereHautDroit.attach()
+	paupiereBasGauche.attach()
+	rotationCylindre.attach()
+	paupiereBasDroit.attach()
+	paupiereHautGauche.attach()
+	berceau.attach()
 	paupiereHautDroit.moveTo(180)
 	paupiereHautGauche.moveTo(180)
 	paupiereBasDroit.moveTo(180)
@@ -27,6 +33,11 @@ def paupiereOuvre():
 	berceau.moveTo(180)
 
 def paupiereFerme():
+	paupiereHautDroit.attach()
+	paupiereBasGauche.attach()
+	rotationCylindre.attach()
+	paupiereBasDroit.attach()
+	paupiereHautGauche.attach()
 	paupiereHautDroit.moveTo(0)
 	paupiereHautGauche.moveTo(0)
 	paupiereBasDroit.moveTo(0)
@@ -35,21 +46,31 @@ def paupiereFerme():
 	berceau.moveTo(0)
 
 def monteSourcil():
+	sourcilDroit.attach()
+	sourcilGauche.attach()
 	sourcilDroit.moveTo(180)
 	sourcilGauche.moveTo(180)
 	
 def baisseSourcil():
+	sourcilDroit.attach()
+	sourcilGauche.attach()
 	sourcilDroit.moveTo(0)
 	sourcilGauche.moveTo(0)
 	
 def regardDroit():
+	oeilDroit.attach()
+	oeilGauche.attach()
 	oeilDroit.moveTo(0)
 	oeilGauche.moveTo(0)
 	
 def regardGauche():
+	oeilDroit.attach()
+	oeilGauche.attach()
 	oeilDroit.moveTo(180)
 	oeilGauche.moveTo(180)
 
 def regardDevant():
+	oeilDroit.attach()
+	oeilGauche.attach()
 	oeilDroit.moveTo(113)
 	oeilGauche.moveTo(98)
